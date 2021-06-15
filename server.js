e009ab25998baebbc1dd7c6a0ef9575e9c82c089
Plan49 <anthonyplanisek1@gmail.com>
2021-06-15 17:03:25 -0400

    added table to display data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,27 +19,44 @@ const connection = mysql.createConnection({
         name: 'startMenu',
         type: 'list',
         message: 'What would like to do? (use arrow keys)',
-        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee'],
+        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'EXIT'],
       })
       .then((answer) => {
-        // based on their answer, either call the bid or the post functions
+        
         if (answer.startMenu === 'View All Employees') {
             console.log("view All")
+            viewAll()
         } else if (answer.startMenu === 'View All Employees By Department') {
             console.log("view D")
+
         } else if (answer.startMenu === 'View All Employees By Manager') {
             console.log("view M")
+
         } else if (answer.startMenu === 'Add Employee') {
             console.log("add")
+
         } else if (answer.startMenu === 'Remove Employee') {
             console.log("remove")
+
         } else {
           connection.end();
         }
       });
   };
 
+const viewAll = () => {
 
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        console.log(res)
+        
+        console.table([
+            res
+        ])
+        
+    })
+
+}
 
 
 
